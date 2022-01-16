b0bf9eb438582a5085c7e8d635c6168b8b43cfec
RichieOrito <richard.omondi@student.moringaschool.com>
2022-01-17 00:25:05 +0300

    This commit creates a constructor function and passed the repo & user properties from the githubapi

--- src/app/repo.ts
+++ src/app/repo.ts
@@ -1,2 +1,5 @@
 export class Repo {
+  constructor( public name: string,public description: string, public url: string){}
+  //inside the constructor function I have passed the repo properties from the github api i'd want displayed
+  //The constructor function enables us to define the initialization logic for creating an object.
 }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,7 @@
 export class User {
+  constructor(public login: string, public name: string, public location: string, public followers: number, 
+    public following: number, public public_repos: number, public created_at: Date, public hireable: string,
+     public avatar_url: string, public html_url: string, public bio: string, public updated_at: Date){}
+     //inside the constructor function I have passed the user properties from the github api i'd want displayed
+     //The constructor function enables us to define the initialization logic for creating an object.
 }
