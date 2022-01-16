f0fe2cc43ff952880df86be92a9056145423a7d0
RichieOrito <richard.omondi@student.moringaschool.com>
2022-01-16 23:34:17 +0300

    This commit adds routing for navigation from one view to another

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResultComponent } from './result/result.component';
+import { SearchComponent } from './search/search.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { componentFactoryName } from '@angular/compiler';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search', component: SearchComponent},
+  { path: 'result', component: ResultComponent},
+  { path: '404', component: NotFoundComponent},
+  { path:'**', component:NotFoundComponent},
 
+  { path: '', redirectTo:'search', pathMatch:'full'}, //I have created an empty path with the redirectTo property that points to the path of the searchComponent.
+  { path: '**', redirectTo:'404', pathMatch:'full'} //I have then defined a route whose path has wildcards and directed this path to the NotFoundComponent.
+];                                                  // The wildcards define a route that is not present in our routes array. So in the case a user tries to 
+                                                    //look up a route that does not exist in the routes of our app, they will be taken to the not-found component
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
