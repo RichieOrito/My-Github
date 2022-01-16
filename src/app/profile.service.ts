c051afe905a61abf942d6d266863c3f21ffde188
RichieOrito <richard.omondi@student.moringaschool.com>
2022-01-17 00:35:20 +0300

    This commit has defined info request method that defines the ApiResponse interface and the promise instance

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -9,7 +9,25 @@ import { Router } from '@angular/router';
 })
 export class ProfileService {
 
-  
+  user!: User;
+  repo!: Repo;
+  repos!: [];
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) { 
+    this.user = new User("", "", "", 0, 0, 0, new Date(), "", "", "", "", new Date());
+    this.repo = new Repo("", "", "");
+    this.router = router;
+  }
+
+  getinfo(username: string){
+    interface ApiResponseUser{
+      login: string, name: string, location:string, followers: number, 
+       following: number, public_repos: number, created_at: Date, hireable: string,
+        avatar_url: string, html_url: string, bio: string, updated_at: Date
+    }
+    interface ApiResponserepo{
+      name: string, description: string, url: string
+    }
+
+  }
 }
