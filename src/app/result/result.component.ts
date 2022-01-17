cf08854c4ce92a02a0802689e86f028d54416829
RichieOrito <richard.omondi@student.moringaschool.com>
2022-01-17 18:03:52 +0300

    This commit generates the logic that will displays the results = description , repos , repo etc

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../user';
+import { ProfileService } from '../profile.service';
+
+import { browserRefresh } from '../app.component';
 
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
   styleUrls: ['./result.component.css']
 })
-export class ResultComponent implements OnInit {
+export class ResultComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  user!: User;
+  repos: any;
+  profileService!: ProfileService;
+  isDoneLoading!: boolean;
+  router: Router;
+  browserRefresh!: boolean;
+
+  constructor(profileService: ProfileService, router: Router) { 
+    this.profileService = profileService;
+    this.router = router;
+  }
+
+  ngAfterViewInit(){
+    this.isDoneLoading = true;
+  }
 
   ngOnInit(): void {
+    this.isDoneLoading = false;
+    this.browserRefresh = browserRefresh;
+    this.user = this.profileService.user;
+    this.repos = this.profileService.repos;
+    if(browserRefresh){
+      this.router.navigate(['../search']);
+    }
   }
 
 }
