0e8c92838b21ef2e22ec8c1d7e40798d5898a4a9
RichieOrito <richard.omondi@student.moringaschool.com>
2022-01-17 18:05:57 +0300

    This commit adds the declariots , imports and the providers service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProfileComponent } from './profile/profile.component';
+import { ProfileService } from './profile.service';
 import { SearchComponent } from './search/search.component';
 import { ResultComponent } from './result/result.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { DateCountPipe } from './date-count.pipe';
+import { StrikethroughDirective } from './strikethrough.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent,
     SearchComponent,
     ResultComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    DateCountPipe,
+    StrikethroughDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ ProfileService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
