680f317ffb1071312220548194b7d47c4b283829
RichieOrito <richard.omondi@student.moringaschool.com>
2022-01-17 17:59:04 +0300

    This commit updates the profileService class I created a property of user,repo and repos we use the getData method to supply the info

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { User } from './user';
 import { Repo } from './repo';
+import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 
 @Injectable({
@@ -9,25 +9,77 @@ import { Router } from '@angular/router';
 })
 export class ProfileService {
 
-  user!: User;
-  repo!: Repo;
-  repos!: [];
+  user: User;
+  repo: Repo;
+  repos: any[] =[];
 
-  constructor(private http: HttpClient, private router: Router) { 
+
+  constructor(private http: HttpClient, private router: Router) {
     this.user = new User("", "", "", 0, 0, 0, new Date(), "", "", "", "", new Date());
-    this.repo = new Repo("", "", "");
+    this.repo = new Repo("", "", 0, "", "");
     this.router = router;
   }
 
-  getinfo(username: string){
+  getData(username: string){
     interface ApiResponseUser{
-      login: string, name: string, location:string, followers: number, 
+      login: string, name: string, location:string, followers: number,
        following: number, public_repos: number, created_at: Date, hireable: string,
-        avatar_url: string, html_url: string, bio: string, updated_at: Date
+        avatar_url: string, html_url: string, bio: string, updated_at: Date,
     }
-    interface ApiResponserepo{
-      name: string, description: string, url: string
+
+    interface ApiResponseRepo{
+  
+      name: string, description: string,  forks_count: number;
+      language: string; svn_url: string;
     }
+
+    let promise: any = new Promise((resolve, reject) => {
+      this.http.get<ApiResponseUser>("https://api.github.com/users/" + username).toPromise().then(response => {
+        this.user.login = response!.login;
+        this.user.name = response!.name;
+        this.user.location = response!.location;
+        this.user.followers = response!.followers;
+        this.user.following = response!.following;
+        this.user.public_repos = response!.public_repos;
+        this.user.created_at = response!.created_at;
+        this.user.hireable = response!.hireable;
+        this.user.avatar_url = response!.avatar_url;
+        this.user.html_url = response!.html_url;
+        this.user.bio = response!.bio;
+        this.user.updated_at = response!.updated_at;
+
+            resolve(response);
+    },error => {
+      let status = error.status;
+      if (status == 404){
+        this.router.navigate(['../404']);
+      }
+      reject(error);
+
+      })
+
+      this.http.get<ApiResponseRepo[]>("https://api.github.com/users/" + username + '/repos').toPromise().then(response =>{
+       for (let i=0; i < response!.length; i++){
+         this.repo.name = response![i].name;
+         this.repo.description = response![i].description;
+         this.repo.forks_count = response![i].forks_count;
+         this.repo.language = response![i].language;
+         this.repo.svn_url = response![i].svn_url;
+         this.repo = new Repo(this.repo.name, this.repo.description, this.repo.forks_count, this.repo.language, this.repo.svn_url);
+         this.repos.push(this.repo);
+       }
+
+       resolve(response);
+      }, error => {
+        let status = error.status;
+        if (status == 404){
+          this.router.navigate(['../404']);
+        }
+        reject(error);
+      });
+      console.log(this.repos)
+      return promise;
+      });
 
   }
 }
