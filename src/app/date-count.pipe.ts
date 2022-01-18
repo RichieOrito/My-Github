b13022aa6f59708256630107771edcfaf1377096
RichieOrito <richard.omondi@student.moringaschool.com>
2022-01-18 10:19:05 +0300

    Updates the dateCount Logic

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -5,18 +5,31 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-transform(value: any): number {
-  let today:Date = new Date(); //get current date and time
-  let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
-  var dateDifference = Math.abs(value - todayWithNoTime) //returns value in miliseconds
-  const secondsInDay = 86400; //60 seconds * 60 minutes in an hour * 24 hours in a day
-  var dateDifferenceSeconds = dateDifference*0.001; //converts miliseconds to seconds
-  var dateCounter = dateDifferenceSeconds/secondsInDay;
-
-  if (dateCounter >= 1 && value > todayWithNoTime){
-    return dateCounter;
-  }else{
-    return 0;
+transform(value: any, args?: any[]): any{
+  if (value) {
+    const seconds = Math.floor((+new Date() - +new Date(value)) / 1000);
+    if (seconds < 29)
+    return 'Just now';
+    const intervals: any = {
+        'year': 31536000,
+        'month': 2592000,
+        'week': 604800,
+        'day': 86400,
+        'hour': 3600,
+        'minute': 60,
+        'second': 1
+    };
+    let result;
+    for (const i in intervals) {
+      result = Math.floor(seconds / intervals[i]);
+      if (result > 0)
+      if (result === 1) {
+        return result + ' ' + i + ' ago';
+      } else {
+        return result + ' ' + i + 's ago';
+      }
+      }
+    }
+    return value;
   }
-}
 }
